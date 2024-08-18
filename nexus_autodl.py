e5c7d0141674894627bf895248461a2ce036ce9e
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2024-08-18 03:44:55 -0400

    Make tkinter GUI

--- nexus_autodl.py
+++ nexus_autodl.py
@@ -1,91 +1,159 @@
-#!/usr/bin/env python
-
-import logging
+import os
 import random
 import re
-import sys
-import time
+from datetime import datetime
 from pathlib import Path
-from typing import Iterator
+from tkinter import (
+    BooleanVar,
+    Button,
+    Checkbutton,
+    DoubleVar,
+    Entry,
+    Frame,
+    IntVar,
+    Label,
+    StringVar,
+    Tk,
+    Text,
+    Toplevel,
+    Scrollbar,
+    filedialog,
+    messagebox,
+)
 
-import click
 import pyautogui
 from PIL import UnidentifiedImageError
-from PIL.Image import Image, open as open_image
-from pyautogui import ImageNotFoundException
-from pyscreeze import Box
+from PIL.Image import open as open_image
+from PIL.ImageFile import ImageFile
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
-
-
-def human_sorted(paths: Iterator[Path]) -> list[Path]:
-    pattern = re.compile("([0-9]+)")
-
-    def key(key: Path) -> tuple[int | str, ...]:
-        return tuple(int(c) if c.isdigit() else c for c in pattern.split(str(key)))
-
-    return sorted(paths, key=key)
+_INTEGER_PATTERN = re.compile("([0-9]+)")
 
 
-@click.command()
-@click.option("--confidence", default=0.7, show_default=True)
-@click.option("--grayscale/--color", default=True, show_default=True)
-@click.option("--min-sleep-interval", default=1, show_default=True)
-@click.option("--max-sleep-interval", default=5, show_default=True)
-@click.option("--templates-path", default=Path.cwd() / "templates", show_default=True)
-def main(
-    confidence: float,
-    grayscale: bool,
-    min_sleep_interval: int,
-    max_sleep_interval: int,
-    templates_path: str,
-) -> None:
-    templates_path_ = Path(templates_path)
-    templates_path_.mkdir(exist_ok=True)
-    templates: dict[Path, Image] = {}
-    for template_path in human_sorted(templates_path_.iterdir()):
-        try:
-            templates[template_path] = open_image(template_path)
-            logging.info(f"Loaded {template_path}")
-        except UnidentifiedImageError:
-            logging.info(f"{template_path} is not a valid image; skipping")
+def _human_sort(key):
+    return tuple(int(c) if c.isdigit() else c for c in _INTEGER_PATTERN.split(str(key)))
 
-    if len(templates) == 0:
-        logging.error(
-            f"No images found in {templates_path_.absolute()}. "
-            f"If this is your first time running, take a screenshot and crop "
-            f"(WIN+S on Windows) the item on the screen you want to click on, "
-            f"placing the result in the {templates_path_.absolute()} directory."
-        )
-        input("Press ENTER to exit.")
-        sys.exit(1)
 
-    while True:
+class NexusAutoDL:
+    def __init__(self, root) -> None:
+        self._root = root
+        self._root.title("Nexus AutoDL")
+        self._root.resizable(False, False)
+
+        self._confidence = DoubleVar(value=0.7)
+        self._grayscale = BooleanVar(value=True)
+        self._min_sleep_seconds = IntVar(value=1)
+        self._max_sleep_seconds = IntVar(value=5)
+        self._templates_path = StringVar(value="templates")
+
+        Label(root, text="Confidence:").grid(row=0, column=0, sticky="w")
+        Entry(root, textvariable=self._confidence).grid(row=0, column=1)
+
+        Label(root, text="Min sleep seconds:").grid(row=1, column=0, sticky="w")
+        Entry(root, textvariable=self._min_sleep_seconds).grid(row=1, column=1)
+
+        Label(root, text="Max sleep seconds:").grid(row=2, column=0, sticky="w")
+        Entry(root, textvariable=self._max_sleep_seconds).grid(row=2, column=1)
+
+        Label(root, text="Templates directory:").grid(row=3, column=0, sticky="w")
+        Entry(root, state="readonly", textvariable=self._templates_path).grid(row=3, column=1)
+        Button(root, text="...", command=self._select).grid(row=3, column=2)
+
+        Label(root, text="Grayscale:").grid(row=4, column=0, sticky="w")
+        Checkbutton(root, variable=self._grayscale).grid(row=4, column=1)
+
+        Button(root, text="Start", command=self._start).grid(row=5, column=0, columnspan=3)
+
+        self._log_text = None
+        self._templates: dict[str, ImageFile] = {}
+
+    def _log(self, message: str) -> None:
+        assert self._log_text is not None
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self._log_text.insert("end", f"[{timestamp}] ", "timestamp")
+        self._log_text.insert("end", f"{message}\n")
+        self._log_text.tag_config("timestamp", foreground="blue")
+        self._log_text.yview_moveto(1)  # Scroll to the end
+
+    def _match(self):
+        if len(self._templates) == 0:
+            self._log(f"No valid images found in {self._templates_path.get()}.")
+            return
+
         screenshot = pyautogui.screenshot()
 
-        for template_path, template_image in templates.items():
-            logging.info(f"Attempting to match {template_path}.")
-            box: Box | None = None
+        for template_path, template_image in self._templates.items():
+            self._log(f"Attempting to match {template_path}.")
+
+            box = None
             try:
                 box = pyautogui.locate(
                     template_image,
                     screenshot,
-                    grayscale=grayscale,
-                    confidence=confidence,
+                    grayscale=self._grayscale.get(),
+                    confidence=self._confidence.get(),
                 )
-            except ImageNotFoundException:
+            except pyautogui.ImageNotFoundException:
                 pass
-            if not isinstance(box, Box):
+
+            if not box:
                 continue
+
             match_x, match_y = pyautogui.center(box)
             pyautogui.click(match_x, match_y)
-            logging.info(f"Matched at ({match_x}, {match_y}).")
+            self._log(f"Matched at ({match_x}, {match_y}).")
             break
 
-        sleep_interval = random.uniform(min_sleep_interval, max_sleep_interval)
-        logging.info(f"Waiting for {sleep_interval:.2f} seconds.")
-        time.sleep(sleep_interval)
+        sleep_interval = random.uniform(self._min_sleep_seconds.get(), self._max_sleep_seconds.get())
+        self._log(f"Waiting for {sleep_interval:.2f} seconds.")
+        self._root.after(int(sleep_interval * 1000), self._match)
+
+    def _select(self) -> None:
+        abspath = filedialog.askdirectory()
+        if not abspath:
+            return
+        relpath = str(Path(abspath).relative_to(Path.cwd()))
+        self._templates_path.set(relpath)
+
+    def _start(self):
+        self._root.withdraw()  # Close the current window
+
+        log_window = Toplevel(self._root)
+        log_window.title("Logging Console")
+        log_window.protocol("WM_DELETE_WINDOW", self._terminate)
+
+        frame = Frame(log_window)
+        frame.pack(padx=10, pady=10, fill="both", expand=True)
+
+        self._log_text = Text(frame, height=20, width=80, wrap="char")
+        self._log_text.pack(side="left", fill="both", expand=True)
+
+        scrollbar = Scrollbar(frame, command=self._log_text.yview)
+        scrollbar.pack(side="right", fill="y")
+        self._log_text.config(yscrollcommand=scrollbar.set)
+
+        templates_path_ = Path(self._templates_path.get())
+        templates_path_.mkdir(exist_ok=True)
+
+        for template_path in sorted(
+            (str(template_path) for template_path in templates_path_.iterdir()),
+            key=_human_sort,
+        ):
+            try:
+                self._templates[template_path] = open_image(template_path)
+                self._log(f"Loaded {template_path}")
+            except IsADirectoryError:
+                self._log(f"Skipping directory {template_path}")
+            except UnidentifiedImageError:
+                self._log(f"Skipping non-image {template_path}")
+
+        self._match()
+
+    def _terminate(self) -> None:
+        self._root.quit()  # Stop the Tkinter event loop
+        self._root.destroy()  # Close all windows and terminate the application
 
 
 if __name__ == "__main__":
-    main()
+    root = Tk()
+    NexusAutoDL(root)
+    root.mainloop()
